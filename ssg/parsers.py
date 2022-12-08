88b88e3228790360acdb5b83fb8e9afb9ea7e445
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2022-12-08 01:02:10 -0500

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -44,7 +44,7 @@ class MarkdownParser():
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(html, path, dest)
+        self.write(path, dest, html)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
